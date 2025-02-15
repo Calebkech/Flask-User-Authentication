d432b2ca89c19378c81f82ea9eefdfcf9d836b95
calebkech <calebkemboi61@gmail.com>
2025-02-15 15:37:43 +0300

    switched to mysql db

--- config.py
+++ config.py
@@ -1,16 +1,7 @@
 from decouple import config
-import os
 
-# Try to get the DATABASE_URL from the environment variables
-DATABASE_URI = config("DATABASE_URL", default=None)
-
-# If DATABASE_URL is not found or it's a PostgreSQL URI but the database is not available, switch to SQLite
-if DATABASE_URI is None or (DATABASE_URI.startswith("postgres://") and not os.getenv("POSTGRES_AVAILABLE", "false").lower() == "true"):
-    DATABASE_URI = "sqlite:///app.db"  # Default to SQLite for testing
-
-# Replace "postgres://" with "postgresql://" if necessary
-if DATABASE_URI.startswith("postgres://"):
-    DATABASE_URI = DATABASE_URI.replace("postgres://", "postgresql://", 1)
+# Get the DATABASE_URL from the environment variables
+DATABASE_URI = config("DATABASE_URL", default="sqlite:///app.db")
 
 
 class Config(object):
--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,19 +1,19 @@
+import uuid
 from datetime import datetime
 from flask_login import UserMixin
 from src import bcrypt, db
 
-
 class User(UserMixin, db.Model):
     __tablename__ = "users"
 
-    id = db.Column(db.Integer, primary_key=True)
-    email = db.Column(db.String, unique=True, nullable=False)
-    username = db.Column(db.String, unique=True, nullable=False)
-    password = db.Column(db.String, nullable=False)
+    id = db.Column(db.String(36), primary_key=True, default=lambda: str(uuid.uuid4()))  # UUID stored as string
+    email = db.Column(db.String(120), unique=True, nullable=False)
+    username = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(255), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
     amount = db.Column(db.Float, nullable=False, default=0)
-    created_by = db.Column(db.String, nullable=False, default='admin')
+    created_by = db.Column(db.String(50), nullable=False, default='admin')
     expiry_date = db.Column(db.DateTime, nullable=True)
 
     def __init__(self, email, username, password, is_admin=False, expiry_date=None, amount=0, created_by='admin'):
@@ -28,4 +28,3 @@ class User(UserMixin, db.Model):
 
     def __repr__(self):
         return f"<email {self.username}>"
-    
